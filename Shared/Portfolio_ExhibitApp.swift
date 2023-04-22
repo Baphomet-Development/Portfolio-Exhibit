4636acaccbdf5973fbad2ee814d9adff13f0b06d
Jason Lacy <jdlacy@gmail.com>
2023-04-21 20:01:45 -0700

    Initial Commit

---
+++ Shared/Portfolio_ExhibitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Portfolio_ExhibitApp.swift
+//  Shared
+//
+//  Created by Jason on 4/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Portfolio_ExhibitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
